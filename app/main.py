9f6cd72bbccb428f99d1f494e98dcf8f0469f145
si570 <111269867+si570@users.noreply.github.com>
2026-09-21 19:41:30 +0000

    Lab 1: add user creation endpoint

--- app/main.py
+++ app/main.py
@@ -1,10 +1,30 @@
-from fastapi import FastAPI
-app = FastAPI(title = "Lab 1 - FastAPI User API")
+from fastapi import FastAPI, HTTPException, status
+
+from app.schema import UserCreate
+
+
+app = FastAPI(title="Lab 1 - FastAPI User API")
+
+users: list[UserCreate] = []
+
 
 @app.get("/health")
 def health():
-    return{"status": "ok"}
+    return {"status": "ok"}
+
 
 @app.get("/hello")
 def hello():
-    return("message":"some message")
+    return {"message": "Hello, World!"}
+
+
+@app.post("/api/users", status_code=status.HTTP_201_CREATED)
+def add_user(new_user: UserCreate):
+    for existing_user in users:
+        if existing_user.user_id == new_user.user_id:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="A user with this user_id already exists",
+            )
+    users.append(new_user)
+    return new_user
